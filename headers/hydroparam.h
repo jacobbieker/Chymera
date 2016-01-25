ff1612e45ff87f823491cdeee63285f3cdabdc3d
Jacob Bieker <jacob@bieker.tech>
2016-01-25 11:48:41 -0800

    Undo changes to header files
    Copied form other files, to reset any problems with changing m_star

--- headers/hydroparam.h
+++ headers/hydroparam.h
@@ -13,20 +13,22 @@
       integer  jmin,jmin1,jmin2
 
 !  High resolution problem.
+!      parameter (jmax=512, jmax1=jmax+1, jmax2=jmax+2)
       parameter (jmax=256, jmax1=jmax+1, jmax2=jmax+2)
       parameter (kmax=64, kmax1=kmax+1, kmax2=kmax+2)
-      parameter (lmax=256, lmax2=lmax/2)
-!
+      parameter (lmax=128,lmax2=lmax/2)
+!      parameter(pot3jmax=512,pot3jmax1=pot3jmax+1,pot3jmax2=pot3jmax+2)
       parameter(pot3jmax=256,pot3jmax1=pot3jmax+1,pot3jmax2=pot3jmax+2)
-      parameter(pot3kmax=128,pot3kmax1=pot3kmax+1,pot3kmax2=pot3kmax+2)
+      parameter(pot3kmax=64,pot3kmax1=pot3kmax+1,pot3kmax2=pot3kmax+2)
 
 !  Minimum radial grid point, for cutting out central star.
       parameter (jmin=2,jmin1=jmin-1,jmin2=jmin-2)
 
 !  Other parameters.
       integer hj,hk,hj1,hj2,hk1,hk2,itable
+!      parameter (hj=512,hk=512,hj1=hj+1,hk1=hk+1,hj2=hj+2,hk2=hk+2)
       parameter (hj=256,hk=256,hj1=hj+1,hk1=hk+1,hj2=hj+2,hk2=hk+2)
-      parameter (lrlmax=64,lrjmax=32,lrkmax=8,lrjmax1=lrjmax+1)
+      parameter (lrlmax=128,lrjmax=64,lrkmax=8,lrjmax1=lrjmax+1)
       parameter (itable=100)
 
       integer, parameter :: TTABLE=400
--- headers/units.h
+++ headers/units.h
@@ -1,14 +1,25 @@
 !    The following units: Mstar, Rdiskau, Tstar are model
 !    dependent.  Must be set by the user.   
 !    The rest are astronomical units.
-!    For the Rossby Wave Instability, mass of star should be 1
+
+!    Tstar is used in RadTran.f to calculate Igamma.
+
+!    Mstar is used in io.f for normilization purposes to calculate Msyscgs,
+!    as well as in hybrid to calculate the accretion time,
+!    as well as in ExternalRoutines.f to calculate tacc.
+
+!    Rstar is used to calculate accretion luminosity in ExternalRoutines.f
+!    as well as in hybrid.f for the same purpose,
+!    as well as in RadTran.f to calculate Igamma.
+
+!    Rdiskau is used in io.f for normalization purposes.
 
       REAL*8 Mstar,Rstar,Rdiskau,Tstar,gridlim,phylim
       REAL*8 Msuncgs,Rsuncgs,AUcgs,sigmacgs,Gcgs,bkmpcgs
-      PARAMETER (Mstar=1d0,Rstar=2d0,Rdiskau=40d0,Tstar=4d3)
+      PARAMETER (Mstar=1.d0,Rstar=1.d0,Rdiskau=1.d2,Tstar=2.d3)
       PARAMETER (Msuncgs=1.989d33, Rsuncgs=6.96d10, AUcgs=1.496d13)
       PARAMETER (sigmacgs=5.670d-5, Gcgs=6.672d-8, phylim=1.d-4) 
-      PARAMETER (bkmpcgs=8.254d7,gridlim=1.d-12)
+      PARAMETER (bkmpcgs=8.254d7,gridlim=1.d-10)
       real*8,parameter::psize=1d1/AUcgs,dust_to_gas=0.01d0,rhoacgs=3d0
       real*8,parameter::poly_factor=4d0
       real*8,parameter::massBin=1.,ebin=0.,abin=100.
@@ -18,16 +29,16 @@
 !    of the starting model.
 !    cq = 3 for AV on, 0 for AV off. 
 !    irtype = 0 for no irr, 1 for irradiation.
-!    ictype is the cooling type: 1 for const. Tcool, 2 for 
-!    Edd. grey atm. only, 3 for diff. aprox., 4 for diff. 
-!    aprox. with shining atm.
+!    ictype is the cooling type: 1 for constant Tcool, 2 for 
+!    Edd grey atmosphere only, 3 for diffusion approx, 4 for diffusion
+!    approx with shining atmosphere.
 !    cct is only used when using type 1.  It is the cooling
 !    time in ORPS.      
 !    tcoollmt is the lower limit of cooling time in ORP.
 !    tirrlmt  is the lower limit of irradiation time in ORP.
 !    Tbgrnd is the lower limit of the temperature
-!    irop is used to spread the irradiation to a few more cells by dividing the
-!    opacities by some number. 
+!    irop is used to spread the irradiation to a few more cells by dividing 
+!    the opacities by some number. 
 !    jirr is the first first j zone to be irradiated.
 !    jcool is the j zone at which cooling starts.  Disabled in most ictypes.
 !    fk = metallicity factor: fraction of standard metallicity to keep.
@@ -39,8 +50,8 @@
 !    1 = pure ortho -- this is not really physical for astro applications
 !    2 = set mixture
 !    3 = equilibrium
-!   -1 = single gamma law
-!    Set ac and bc for mixture (ac: para component, bc: ortho component)
+!    -1 = single gamma law
+!    Be sure to set ac and bc for mixture (ac: para component, bc: ortho component)
 !    also pick a metallicity by adjusting xabun, yabun, and zabun.
 
 
@@ -59,14 +70,13 @@
       
       INTEGER ictype,irtype,jcool,jirr,SETUNITS,H2STAT
       integer,parameter::JCONST=80
-      PARAMETER(torp=54.2d0,cct=3.0d0,cq=3d0,tcoollmt=14d0,theatlmt=5d1)
+      PARAMETER(torp=17.1d0,cct=2d0,cq=3d0,tcoollmt=5d1,theatlmt=5d1)
       PARAMETER(ictype=0,irtype=0,jcool=2)
-!      PARAMETER(ictype=1,irtype=0,jcool=50)
-      PARAMETER(jirr=10,irop=1.,Tbgrnd=3d0)
-      PARAMETER(tenvk=50d0,fk=1d0)
+      PARAMETER(jirr=11,irop=1.0,Tbgrnd=3d0)
+      PARAMETER(tenvk=150d0,fk=1d0)
       PARAMETER(SETUNITS=0,H2STAT=-1) 
       logical,parameter::find_eos_rho=.true.
-      PARAMETER(amp0=0.0001d0) ! amplitude of initial, random perturbation
+      PARAMETER(amp0=0.005) ! amplitude of initial, random perturbation
 
 #if EXTERNAL_POT>0
       logical, parameter :: external_pot = .true.
