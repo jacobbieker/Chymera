03d3a400946af669da1870c0fd96339f76c86650
Jacob Bieker <jacob@bieker.tech>
2016-03-01 09:42:02 -0800

    Add type parameter and simple check

--- rossby-wave-calc/model_generator.py
+++ rossby-wave-calc/model_generator.py
@@ -1,13 +1,16 @@
 __author__ = 'jacob'
+import csv
+import os
 
-def generate_fort_2(polytropic_index, model, jmax, kmax, jout, kout, log_central_density, iteration, mass_star, xcut, xwidth, xnorm):
-    '''
-
+def generate_fort_2(polytropic_index, model, jmax, kmax, jout, kout, log_central_density, iteration, mass_star, xcut,
+                    xwidth, xnorm, type):
+    """
+    Generate fort.2 model file for CHYMERA Code
     :param polytropic_index: Polytropic index of star
     :param model: The star/disk model, 100 for star/disk, -2.0 for disk with point mass star
     :param jmax: radial grid size
     :param kmax: vertical grid size
-    :param jout: radial size of star+disk
+    :param jout: radial size of star+disk. negative for just the disk
     :param kout: polar radius of star
     :param log_central_density: log of central density
     :param iteration: iteration parameter for model
@@ -15,5 +18,12 @@ def generate_fort_2(polytropic_index, model, jmax, kmax, jout, kout, log_central
     :param xcut: width of star in model
     :param xwidth: percentage of mass in the disk
     :param xnorm: TODO: Figure these x things out
+    :param type: Currently either 'RWI' for Rossby Wave Instability, anything else for general disk model
     :return: A model file for input into the CHYMERA computational fluid dynamics code to run
-    '''
+    """
+
+    if type == 'RWI':
+        print('Using Rossby Wave equations for disk')
+
+    else:
+        print('Using normal equations for disk')
